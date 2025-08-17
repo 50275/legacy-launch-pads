b6dd2be84fd7f370026807e53a54bf3d10a7941a
50275 <68676339+50275@users.noreply.github.com>
2025-08-17 02:11:10 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,16 @@
 Events.on(ContentInitEvent, e => { 
+    // Enable v7 functionality
     Planets.serpulo.campaignRules.legacyLaunchPads = true;
+
+    // Add tech node after advanced launch pad
     var parent = TechTree.all.find(u => u.content == Blocks.advancedLaunchPad); 
     var content = Blocks.launchPad;
-    var cost = Blocks.conveyor.requirements;  
-    new TechTree.TechNode(parent, content, cost);
+    var cost = ItemStack.empty;  
+    var node = new TechTree.TechNode(parent, content, cost); 
 });
+
+/*
+Events.on(WorldLoadEndEvent, e => {
+    Planets.serpulo.campaignRules.legacyLaunchPads = Vars.state.isCampaign();
+}); 
+*/
